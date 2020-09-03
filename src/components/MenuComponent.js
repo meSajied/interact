6dd675b314e19975fa350798cc668e03f1adfa32
meSajied <sajiedshahyousuf@gmail.com>
2020-09-03 22:29:16 +0600

    updated: menucomponent and gitignore

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,62 +1,67 @@
-import React from "react";
-import { Card, CardImg, CardImgOverlay,
-	CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+	CardTitle } from 'reactstrap';
 
+class Menu extends Component {
 
-function RednerMenuItem({dish, onClick}) {
-	return(
-		<Card>
-			<Link to={`/menu/${dish.id}`} >
-				<CardImg width="100%" src={dish.image} alt={dish.name} />
-				<CardImgOverlay>
-					<CardTitle>{dish.name}</CardTitle>
-				</CardImgOverlay>
-			</Link>
-		</Card>
-	);
-}
+	constructor(props) {
+		super(props);
 
-const Menu = (props) => {
+		this.state = {
+			selectedDish: null
+		}
+	}
 
-	const menu = props.dishes.dishes.map((dish) =>{
+	onDishSelect(dish) {
+		this.setState({ selectedDish: dish});
+	}
 
-		return(
-			<div key={dish.id} className="col-12 col-md-5 m-1">
-				<RednerMenuItem dish={dish}/>
-			</div>
-		)
-	});
+	renderDish(dish) {
+		if (dish != null)
+			return(
+				<Card>
+					<CardImg top src={dish.image} alt={dish.name} />
+					<CardBody>
+						<CardTitle>{dish.name}</CardTitle>
+						<CardText>{dish.description}</CardText>
+					</CardBody>
+				</Card>
+			);
+		else
+			return(
+				<div></div>
+			);
+	}
 
-	if (props.dishes.errMess) {
-		return(
+	render() {
+		const menu = this.props.dishes.map((dish) => {
+			return (
+				<div  className="col-12 col-md-5 m-1">
+					<Card key={dish.id}
+					      onClick={() => this.onDishSelect(dish)}>
+						<CardImg width="100%" src={dish.image} alt={dish.name} />
+						<CardText src={dish.comment} />
+						<CardImgOverlay>
+							<CardTitle>{dish.name}</CardTitle>
+						</CardImgOverlay>
+					</Card>
+				</div>
+			);
+		});
+
+		return (
 			<div className="container">
 				<div className="row">
-					<div className="col-12">
-						<h4>{props.dishes.errMess}</h4>
+					{menu}
+				</div>
+				<div className="row">
+					<div  className="col-12 col-md-5 m-1">
+						{this.renderDish(this.state.selectedDish)}
 					</div>
 				</div>
 			</div>
 		);
 	}
-	else
-		return(
-			<div className="container">
-				<div className="row">
-					<Breadcrumb>
-						<BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-						<BreadcrumbItem active>Menu</BreadcrumbItem>
-					</Breadcrumb>
-					<div className="col-12">
-						<h3>Menu</h3>
-						<hr />
-					</div>
-				</div>
-				<div className="row">
-					{menu}
-				</div>
-			</div>
-		);
 }
 
 export default Menu;
