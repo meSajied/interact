4467251085ceef86ee1bbffa257604fe01ca7427
meSajied <sajiedshahyousuf@gmail.com>
2020-09-24 19:37:50 +0600

    added:manipulate dom and sliding animation...

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { postComment, addComment, fetchDishes, fetchComments,
 	fetchPromos } from '../redux/ActionCreators';
 import Menu from './MenuComponent';
@@ -99,18 +100,25 @@ class Main extends Component {
 			<div>
 				<Header />
 				<div>
-					<Switch>
-						<Route path='/home' component={HomePage} />
-						<Route exact path='/aboutus' component={() =>
-							<About leaders={this.props.leaders} />} />} />
-						<Route exact path='/menu' component={() =>
-							<Menu dishes={this.props.dishes} />} />
-						<Route path='/menu/:dishId' component={DishWithId} />
-						// added feedback form...
-						<Route exact path='/contactus' component={() =>
-							<Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-						<Redirect to="/home" />
-					</Switch>
+					{/* manipulating dom with small animation*/}
+					<TransitionGroup>
+						{/* A animation library for slide animation.... */}
+						<CSSTransition key={this.props.location.key}
+				               classNames="page" timeout={300}>
+						<Switch>
+							<Route path='/home' component={HomePage} />
+							<Route exact path='/aboutus' component={() =>
+								<About leaders={this.props.leaders} />} />} />
+							<Route exact path='/menu' component={() =>
+								<Menu dishes={this.props.dishes} />} />
+							<Route path='/menu/:dishId' component={DishWithId} />
+							// added feedback form...
+							<Route exact path='/contactus' component={() =>
+								<Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+							<Redirect to="/home" />
+						</Switch>
+						</CSSTransition>
+					</TransitionGroup>
 				</div>
 				<Footer />
 			</div>
